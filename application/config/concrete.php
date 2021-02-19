4c56cb4c6efd94f79a855ac4cf2151e5b543172e
Katz Ueno <katz@concrete5.co.jp>
2021-02-19 22:39:26 +0900

    Initial commit

---
+++ application/config/concrete.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    /*
+     * ------------------------------------------------------------------------
+     * Debug settings
+     * ------------------------------------------------------------------------
+     */
+    'debug' => [
+        /*
+         * Display errors / エラー表示
+         *
+         * @var bool
+         */
+        'display_errors' => true,
+
+        /*
+         * Site debug level / デバッグ表示
+         *
+         * @var string (message|debug)
+         */
+        'detail' => 'debug',
+
+        /*
+         * Error reporting level レポートレベル
+         *
+         * @var int|null
+         */
+        'error_reporting' => null,
+    ],
+];
---
+++ readme.md
@@ -0,0 +1,13 @@
+# concrete5 config: Debug Mode
+
+This is concrete5 config override to display error debug message.
+
+Please upload `application/config/concrete.php` file to your concrete5 site in order to force display error message in case of emergency.
+
+Please make sure to remove the config file once you reolve the issue.
+
+concrete5 サイトで、エラー時のデバッグを強制的に表示させるようにするための config ファイルのオーバーライドです。
+
+例えばエラーが表示されて何も確認できない場合、この application/config/concrete.php` ファイルをサーバーにアップロードしてください。
+
+また解決した後、config/concrete.php ファイルを削除するのを忘れずにお願いします。
